25ff38447c5492c7b456bc2bb35aea6bdb505826
Roshan Jacob <roshan.jacob2021@gmail.com>
2020-07-12 11:24:19 -0500

    .gitignore

---
+++ src/main/java/com/codeup/blogpractice/BlogPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.codeup.blogpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogPracticeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/codeup/blogpractice/BlogPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeup.blogpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
