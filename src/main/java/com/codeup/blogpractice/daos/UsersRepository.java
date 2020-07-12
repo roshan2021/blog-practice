0320c3a2b243d989251ebf8cb492db9882ea5961
Roshan Jacob <roshan.jacob2021@gmail.com>
2020-07-12 18:09:50 -0500

    models

---
+++ src/main/java/com/codeup/blogpractice/daos/UsersRepository.java
@@ -0,0 +1,8 @@
+package com.codeup.blog.daos;
+
+import com.codeup.blog.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UsersRepository extends JpaRepository<User, Long> {
+    User findByUsername(String username);
+}
--- src/main/java/com/codeup/blogpractice/models/Ad.java
+++ src/main/java/com/codeup/blogpractice/models/Ad.java
@@ -1,12 +1,48 @@
-package com.codeup.blogpractice.models;
+package com.codeup.blog.models;
 
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+//Table is a representation of many ads
+@Table(name = "ads")
+//representation of ONE Ad
 public class Ad {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false, length = 200)
     private String title;
+
+    @Column(nullable = false)
     private String description;
 
-    public Ad(String title, String description) {
+    @OneToOne
+    private User owner;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "ad")
+    private List<AdImage> images;
+
+    // spring framework uses this empty constructor
+    public Ad() {}
+
+    //Create AdImages [without id]
+    public Ad(String title, String description, User owner, List<AdImage> images) {
         this.title = title;
         this.description = description;
+        this.owner = owner;
+        this.images = images;
+    }
+
+    //Read AdImages [with id]
+    public Ad(long id, String title, String description, User owner, List<AdImage> images) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.owner = owner;
+        this.images = images;
     }
 
     public String getTitle() {
@@ -24,4 +60,30 @@ public class Ad {
     public void setDescription(String description) {
         this.description = description;
     }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public User getOwner() {
+        return owner;
+    }
+
+    public void setOwner(User owner) {
+        this.owner = owner;
+    }
+
+    public List<AdImage> getImages() {
+        return images;
+    }
+
+    public void setImages(List<AdImage> images) {
+        this.images = images;
+    }
+
 }
+
