d71cbffa1c25515e206ae4a7374622d6da33fb1c
Roshan Jacob <roshan.jacob2021@gmail.com>
2020-07-12 13:34:22 -0500

    join Request Param update

--- src/main/java/com/codeup/blogpractice/controllers/HelloController.java
+++ src/main/java/com/codeup/blogpractice/controllers/HelloController.java
@@ -2,22 +2,31 @@ package com.codeup.blogpractice.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class HelloController {
-//    @GetMapping("/hello")
-//    @ResponseBody
-//    public String hello(){
-//        return "Hello from spring";
-//    }
+    @GetMapping("/hello")
+    @ResponseBody
+    public String hello(){
+        return "Hello from spring";
+    }
 
     @GetMapping("/hello/{name}")
     public String sayHello(@PathVariable String name, Model model){
         model.addAttribute("sayHelloName", name);
         return "hello";
+    }
+
+    @GetMapping("/join")
+    public String showJoinForm(){
+        return "join";
+    }
+
+    @PostMapping("/join")
+    public String joinCohort(@RequestParam String cohort, Model model){
+        model.addAttribute("cohort", cohort);
+        return "join";
     }
 
 }
